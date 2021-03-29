1aaa6ee50073b4b362533a086014970319e9857a
Alex <js.fullstack.development@gmail.com>
2021-03-29 20:42:02 +0300

    test commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div></div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Feedback</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
